2db3b1405f06cb908462b9f1569afa40bd9f16de
Jet Fontanilla <jetlogs@gmail.com>
2023-02-09 11:29:10 +0900

    prevent image loading race conditions

--- model.js
+++ model.js
@@ -5,10 +5,23 @@ canvas.height = 512;
 const ctx = canvas.getContext("2d");
 
 function drawImage(imageId) {
-    const image = document.getElementById(imageId);
-    ctx.drawImage(image, 0, 0)
+    return new Promise(resolve => {
+        const image = document.getElementById(imageId);
+        
+        if (image.complete) {
+            ctx.drawImage(image, 0, 0);
+            return resolve();
+        }
+
+        image.onload = () => {
+            ctx.drawImage(image, 0, 0);
+            resolve();
+        }
+    });
 }
 
-drawImage("body");
-drawImage("eye-l");
-drawImage("eye-r");
+(async function drawBase() {
+    await drawImage("body");
+    await drawImage("eye-l");
+    await drawImage("eye-r");
+})();
