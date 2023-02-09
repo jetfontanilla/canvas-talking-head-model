88544f20ba9f4f51f6c2c70435dc70050c8e5388
Jet Fontanilla <jetlogs@gmail.com>
2023-02-09 14:41:08 +0900

    viseme sample integration

--- model.js
+++ model.js
@@ -1,8 +1,5 @@
-const canvas = document.getElementById("canvas");
-canvas.width = 512;
-canvas.height = 512;
-
-const ctx = canvas.getContext("2d");
+let canvas;
+let ctx;
 
 function drawImage(imageId) {
     return new Promise(resolve => {
@@ -20,8 +17,69 @@ function drawImage(imageId) {
     });
 }
 
-(async function drawBase() {
+document.addEventListener("DOMContentLoaded", async function() {
+    canvas = document.getElementById("canvas");
+    canvas.width = 512;
+    canvas.height = 512;
+
+    ctx = canvas.getContext("2d");
+
     await drawImage("body");
     await drawImage("eye-l");
     await drawImage("eye-r");
-})();
+});
+
+// Viseme ID lookup
+const frameLookup = {
+    0: "mouth-a",
+    1: "mouth-d",
+    2: "mouth-h",
+    3: "mouth-e",
+    4: "mouth-c",
+    5: "mouth-c",
+    6: "mouth-b",
+    7: "mouth-f",
+    8: "mouth-c",
+    9: "mouth-d",
+    10: "mouth-e",
+    11: "mouth-h",
+    12: "mouth-c",
+    13: "mouth-e",
+    14: "mouth-c",
+    15: "mouth-b",
+    16: "mouth-f",
+    17: "mouth-c",
+    18: "mouth-g",
+    19: "mouth-b",
+    20: "mouth-d",
+    21: "mouth-x"
+};
+
+function drawMouthFrame(frameId) {
+    ctx.fillStyle = "rgb(09, 81, 74)";
+    ctx.fillRec(195, 165, 110, 75);
+
+    const assetId = frameLookup[frameId];
+    drawImage(assetId);
+}
+
+const ttsAudio = new Audio("tts.wav");
+async function playAudio() {
+    fetch(new Request("viseme.json"), {
+        method: "GET",
+        mode: "no-cors"
+    }).then(response => {
+        console.log(response);
+        const visemeData = response.text();
+        console.log(visemeData);
+    });
+    
+
+    ttsAudio.play();
+
+    const PRECISION = 50; //in ms
+    const intervalId = setInterval(() => {
+
+    }, 50);
+    clearInterval(intervalId);
+}
