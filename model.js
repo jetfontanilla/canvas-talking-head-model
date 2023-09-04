10473bf4631e55551b5b39ecc7968455f12bf503
Jet Fontanilla <jetlogs@gmail.com>
2023-09-04 10:46:17 +0900

    mouth v2

--- model.js
+++ model.js
@@ -103,7 +103,7 @@ async function drawMouthFrame(frameId) {
 
     ctx.fillStyle = `rgb(90, 81, 74)`;
     ctx.fillRect(200, 165, 100, 75);
-    ctx.drawImage(image, 200, 165);
+    ctx.drawImage(image, 220, 185);
 }
 
 let ttsAudio;
